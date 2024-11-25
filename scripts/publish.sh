4d6b4ab396cd0e1145c92d7cf3c2fb32e429079c
Arnau Sanchez <pyarnau@gmail.com>
2024-11-25 12:15:05 +0100

    Customize publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e -u -o pipefail
 
-version=$(cat package.json | jq -r '.version')
+version=$(jq <package.json -r '.version')
 publish_opts=$(if echo "$version" | grep -q beta; then echo "--tag beta"; fi)
 
-yarn build
-yarn publish $publish_opts --new-version "$version" build/
+yarn build-lib
+# shellcheck disable=SC2086
+yarn publish $publish_opts --new-version "$version" dist/
 
 git tag "v$version" -f
 git push --tags
