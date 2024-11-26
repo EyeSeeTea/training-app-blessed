4ef6aa4e3936b120c9b3833727453b0c5934e0e2
Adrian Quintana <adrianquintana@gmail.com>
2024-11-26 09:45:26 +0000

    pass public access parameter to npm publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,7 +6,7 @@ publish_opts=$(if echo "$version" | grep -q beta; then echo "--tag beta"; fi)
 
 yarn build-lib
 # shellcheck disable=SC2086
-yarn publish $publish_opts --new-version "$version" dist/
+yarn publish $publish_opts --new-version "$version" --access=public dist/
 
 git tag "v$version" -f
 git push --tags
