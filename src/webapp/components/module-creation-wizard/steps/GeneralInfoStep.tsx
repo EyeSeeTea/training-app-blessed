da40f86a2aeddeb8425043e7de1eec5836bd22e1
Adrian Quintana <adrianquintana@gmail.com>
2025-01-16 08:57:18 +0000

    lint

--- src/webapp/components/module-creation-wizard/steps/GeneralInfoStep.tsx
+++ src/webapp/components/module-creation-wizard/steps/GeneralInfoStep.tsx
@@ -184,5 +184,5 @@ const dhisVersions = [
     { value: "2.38", text: "2.38" },
     { value: "2.39", text: "2.39" },
     { value: "2.40", text: "2.40" },
-    { value: "2.41", text: "2.41" }
+    { value: "2.41", text: "2.41" },
 ];
