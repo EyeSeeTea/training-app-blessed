c9e3e5ce7550e0b4d85f3c8209b565988b648a1b
gqcorneby <geonina@eyeseetea.com>
2025-02-19 16:44:33 +0800

    use Translations

--- src/domain/usecases/ExtractTranslationsUseCase.ts
+++ src/domain/usecases/ExtractTranslationsUseCase.ts
@@ -1,11 +1,11 @@
 import { UseCase } from "../../webapp/CompositionRoot";
 import { TranslableTextRepository } from "../repositories/TranslableTextRepository";
-import { buildTranslationMap } from "../entities/TranslatableText";
+import { buildTranslationMap, Translations } from "../entities/TranslatableText";
 
 export class ExtractTranslationsUseCase implements UseCase {
     constructor(private translationRepository: TranslableTextRepository) {}
 
-    public async execute(id?: string): Promise<Record<string, Record<string, string>>> {
+    public async execute(id?: string): Promise<Translations> {
         const texts = await this.translationRepository.extractTranslations(id);
         return buildTranslationMap(texts);
     }
